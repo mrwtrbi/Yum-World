a228e4b33712c9a9fa2c97d669b836c2168c001a
Eléonore <81861914+eleonoree@users.noreply.github.com>
2023-03-28 18:47:38 +0200

    Update index.php

--- php/index.php
+++ php/index.php
@@ -4,9 +4,12 @@
 	<head>
 		<meta charset="utf-8" />
 		<title>Yum World</title>
+		<link rel="shortcut icon" href="../img/Logo.png" type="image/x-icon">
 		<link rel="stylesheet" href="../css/style.css">	
 		<link rel="stylesheet" href="../css/header.css">
 		<link rel="stylesheet" href="../css/footer.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+		
 </head>
 
 
@@ -20,13 +23,13 @@
 <main>
 	<?php
 		//include_once("mainMenu.php");
+		
 	?>
 		
 	<div class="main-text">	
 		<p class="main-content">
 Bienvenue sur Yum World !
 		</p>
-		<img id="photo" src="../img/Logo.png" alt="Logo">
 
 	</div>
 </main>
