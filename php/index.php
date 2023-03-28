ac54742d09a05a9edc91dd1ef3ec49bed0b587c8
Eléonore <81861914+eleonoree@users.noreply.github.com>
2023-03-28 10:34:08 +0200

    Create index.php

---
+++ php/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="utf-8" />
+		<title>Yum World</title>
+		<link rel="stylesheet" href="../css/style.css">	
+		<link rel="stylesheet" href="../css/header.css">
+		<link rel="stylesheet" href="../css/footer.css">
+</head>
+
+
+<body>
+
+	<?php
+		include_once("header.php");
+	?>
+
+
+<main>
+	<?php
+		//include_once("mainMenu.php");
+	?>
+		
+	<div class="main-text">	
+		<p class="main-content">
+Bienvenue sur Yum World !
+		</p>
+		<img id="photo" src="../img/Logo.png" alt="Logo">
+
+	</div>
+</main>
+
+	<?php
+		include_once("footer.php");
+	?>
+
+</body>
+
+</html>
