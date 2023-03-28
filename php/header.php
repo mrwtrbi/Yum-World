c1f84f6adf83ca3a0383194f40277a2fe94f318b
Eléonore <81861914+eleonoree@users.noreply.github.com>
2023-03-28 10:34:45 +0200

    Create header.php

---
+++ php/header.php
@@ -0,0 +1,21 @@
+<header>
+	<div class="header-banniere">
+		<img id="header-banniere-logo" src="../img/Logo.png" alt="Logo">
+		<h2 id="header-banniere-titre">Yum World</h2>
+	</div>
+		
+	<div class="header-menu">
+		<ul>	
+			<li> <a href="index.php"> <i class="fa fa-house"></i>Accueil</a></li>
+		
+			<li><a>Afrique</a></li>
+			<li><a>Amérique</a></li>
+			<li><a>Asie</a></li>
+			<li><a>Europe</a></li>
+			<li><a>Océanie</a></li>
+			
+			<li><a>Contact</a></li>
+			
+		</ul>
+	</div>
+</header>
