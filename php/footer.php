57ee9bc82c936790dc507dbb4ef310d8b561bad7
Eléonore <81861914+eleonoree@users.noreply.github.com>
2023-03-28 10:35:19 +0200

    Create footer.php

---
+++ php/footer.php
@@ -0,0 +1,34 @@
+<footer>
+<div class="footer-container">
+	<div class="colonne">
+	<h3>Plan du site</h3>
+	<ul>
+		<li>Afrique</li>
+		<li>Amérique</li>
+		<li>Asie</li>
+		<li>Europe</li>
+		<li>Océanie</li>
+	</ul>
+	</div>
+
+	<div class="colonne">
+	<h3>Mentions légales</h3>
+	<ul>
+		<li><a href="#">Politique de confidentialité</a></li>
+		<li><a href="#">Hébergeur du site</a></li>
+		<li><a href="#">Concepteur du site</a></li>
+	</ul>
+	</div>
+
+	<div class="colonne">
+	<h3>Nous contacter</h3>
+	<ul>
+		<li><a href="#">Concepteur</a></li>
+		<li><a href="#">Société</a></li>
+		<li><a href="#">Yum World</a></li>
+	</ul>
+	</div>
+</div> 
+
+<p id="copyright">Yum World © 2023</p>
+</footer>
