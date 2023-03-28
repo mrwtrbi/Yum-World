6285fe8e65d26ab7a1fe342e44994724adcb7280
Eléonore <81861914+eleonoree@users.noreply.github.com>
2023-03-28 18:48:04 +0200

    Update header.php

--- php/header.php
+++ php/header.php
@@ -2,19 +2,21 @@
 	<div class="header-banniere">
 		<img id="header-banniere-logo" src="../img/Logo.png" alt="Logo">
 		<h2 id="header-banniere-titre">Yum World</h2>
+		<a href="#" class="header-banniere-images"><i class="fas fa-cart-shopping"></i> Panier</a>
+		<a href="#" class="header-banniere-images"><i class="fas fa-cart-shopping"></i> Connexion</a>
 	</div>
 		
 	<div class="header-menu">
 		<ul>	
-			<li> <a href="index.php"> <i class="fa fa-house"></i>Accueil</a></li>
+			<li> <a href="index.php"><i class="fas fa-house"></i> Accueil</a></li>
 		
-			<li><a>Afrique</a></li>
-			<li><a>Amérique</a></li>
-			<li><a>Asie</a></li>
-			<li><a>Europe</a></li>
-			<li><a>Océanie</a></li>
+			<li><a href="#">Afrique</a></li>
+			<li><a href="#">Amérique</a></li>
+			<li><a href="#">Asie</a></li>
+			<li><a href="#">Europe</a></li>
+			<li><a href="#">Océanie</a></li>
 			
-			<li><a>Contact</a></li>
+			<li><i class="fa-solid fa-envelope"></i><a> Contact</a></li>
 			
 		</ul>
 	</div>
