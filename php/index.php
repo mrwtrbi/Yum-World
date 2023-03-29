a8f4c622a4020ff07f3d0131b61726771d5d011c
Eléonore <81861914+eleonoree@users.noreply.github.com>
2023-03-29 15:49:39 +0200

    Update index.php

--- php/index.php
+++ php/index.php
@@ -9,7 +9,7 @@
 		<link rel="stylesheet" href="../css/header.css">
 		<link rel="stylesheet" href="../css/footer.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-		
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lobster Two">
 </head>
 
 
