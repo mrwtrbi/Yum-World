b9000da33394308dfee3a6061eeda95143ebff70
mrwtrbi <99337265+mrwtrbi@users.noreply.github.com>
2023-03-29 15:47:57 +0200

    Add files via upload

--- php/header.php
+++ php/header.php
@@ -8,15 +8,15 @@
 		
 	<div class="header-menu">
 		<ul>	
-			<li> <a href="index.php"><i class="fas fa-house"></i> Accueil</a></li>
+			<li id="accueil"> <a href="index.php"><i class="fas fa-house"></i> Accueil</a></li>
 		
-			<li><a href="#"><i class="fa-solid fa-earth-africa"></i> Afrique</a></li>
-			<li><a href="#"><i class="fa-solid fa-earth-americas"></i> Amérique</a></li>
-			<li><a href="#"><i class="fa-solid fa-earth-asia"></i> Asie</a></li>
-			<li><a href="#"><i class="fa-solid fa-earth-europe"></i> Europe</a></li>
-			<li><a href="#"><i class="fa-solid fa-earth-oceania"></i> Océanie</a></li>
+			<li id="afrique"><a href="#"><i class="fa-solid fa-earth-africa"></i> Afrique</a></li>
+			<li id="amerique"><a href="#"><i class="fa-solid fa-earth-americas"></i> Amérique</a></li>
+			<li id="asie"><a href="#"><i class="fa-solid fa-earth-asia"></i> Asie</a></li>
+			<li id="europe"><a href="#"><i class="fa-solid fa-earth-europe"></i> Europe</a></li>
+			<li id="oceanie"><a href="#"><i class="fa-solid fa-earth-oceania"></i> Océanie</a></li>
 			
-			<li><i class="fa-solid fa-envelope"></i><a> Contact</a></li>
+			<li id="contact"><a href="#"><i class="fa-solid fa-envelope"></i> Contact</a></li>
 			
 		</ul>
 	</div>
