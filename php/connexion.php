5b016fa84caf8b141fccca744802922f1f5a6bfd
destineeMouely <127632700+destineeMouely@users.noreply.github.com>
2023-05-01 21:17:46 +0200

    Add files via upload

---
+++ php/connexion.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <?php
+		include_once("head.php");
+	?>
+<head>
+	<title>Page de connexion</title>
+    <script src="../js/connexion.js"></script>
+</head>
+<body>
+    
+    <?php
+        include_once("header.php");
+    ?>
+    
+    <div id=form>
+	<h1>Connexion</h1>
+	<form  action="../php/connexion.php" id="connexion-form">
+		<label>Nom d'utilisateur ou adresse e-mail:</label><br>
+		<input type="text" id="username" name="username" ><br>
+		<label>Mot de passe:</label><br>
+        <div>
+		<input type="password" id="password" name="password">
+        
+        <i class="fas fa-eye toggle-password" ></i><br><br>
+        </div>
+		<input type="submit" value="Connexion">
+        
+	</form>
+	<p><a href="../php/motDePasseOublie.php">Mot de passe oublié?</a></p></br>
+    <p>Ou</p></br>
+    <p><a href="../php/inscription.php">Créer un compte</a></p>
+    </div>
+    <?php
+        include_once("footer.php");
+    ?>
+    
+</body>
+</html>
