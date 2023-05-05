013c7705640ee008e59287850434db30cc879f2c
destineeMouely <127632700+destineeMouely@users.noreply.github.com>
2023-05-05 22:20:16 +0200

    Update header.php

--- php/header.php
+++ php/header.php
@@ -3,7 +3,7 @@
 		<img id="header-banniere-logo" src="../img/Logo.png" alt="Logo">
 		<h2 id="header-banniere-titre">Yum World</h2>
 		<a href="panier.php" class="header-banniere-images"><i class="fas fa-cart-shopping"></i> Panier</a>
-		<a href="#" class="header-banniere-images"><i class="fa-solid fa-user"></i> Connexion</a>
+		<a href="../php/connexion.php" class="header-banniere-images"><i class="fa-solid fa-user"></i> Connexion</a>
 	</div>
 		
 	<div class="header-menu">
