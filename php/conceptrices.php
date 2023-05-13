5c95e8905e716dd35edc731d558c3970ed6f4a44
mrwtrbi <99337265+mrwtrbi@users.noreply.github.com>
2023-05-13 19:18:52 +0200

    Add files via upload

--- php/conceptrices.php
+++ php/conceptrices.php
@@ -12,15 +12,31 @@
         include_once("header.php");
     ?>
     
-    <div id="concept">
-        <br><br><br><br><br><br><br><br><br><br>
-        <img id="" src=""></img>
-        <p>Nous voici, conceptrices du site Yum World !<p>
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-    </div>
-        
+    <div class="conceptrices">
+        <div class="image-conceptrices">
+            <img src="../img/Conceptrices.png" alt="Conceptrices">
+        </div>
 
+        <div class="text-conceptrices">
+            <br><br><br><br><br><br><br><br><br><br><br>
+            <p>Ce site web a été créé par trois étudiantes en première année de cycle ingénieur en Mathématiques
+                 appliquées dans l'école CY Tech.</p>
+                 <br>
+                 <br>
 
+            <ul>
+                <li>Eléonore, s'est occupée de l'architecture du site et de sa forme.</li><br>
+                <li>Destinée, s'est occupée du formulaire de connexion, de l'émission de la facture et du panier.</li><br>
+                <li>Marwa, s'est occupée de la base de données et du contenu des pages du site.</li><br>
+            </ul>
+            <br><br><br>
+
+            <div class="logo-cy-tech">
+            <img src="../img/Logo-CY-Tech.png" alt="CY-Tech">
+            <br><br><br>
+            </div>            
+        </div>
+    </div>       
 
     <?php
         include_once("footer.php");
